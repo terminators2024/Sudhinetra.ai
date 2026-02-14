bedc3d6c95688ff1ac41de148a76a444db58709e
Terminators2.0 <terminatorsavcoe@gmail.com>
2026-02-14 23:26:59 +0530

    Delete backend directory

--- backend/backend/post_office/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import PostOffice
-
-class PostOfficeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=PostOffice
-        fields="__all__"
-    
-        
